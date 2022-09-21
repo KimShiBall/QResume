96f863a24976473831b976f6b14b5f80dbc18afe
Zombiepro89 <corymyers2003@gmail.com>
2022-09-21 17:54:55 -0500

    Added signup form which communicates with our mongo database.

--- index.js
+++ index.js
@@ -5,12 +5,18 @@
  */
  const express = require("express");
  const path = require("path");
+ const bodyParser = require("body-parser");
+ const mongoose = require("mongoose");
+ require("./models/Registration");
+ require("dotenv").config();
 
 /**
  * App Variables
  */
  const app = express();
  const port = process.env.PORT || "8000";
+ const { check, validationResult} = require('express-validator');
+ const Registration = mongoose.model("Registration");
 
 /**
  *  App Configuration
@@ -19,7 +25,16 @@
 
  app.set("views", path.join(__dirname, "views"));
  app.set("view engine", "pug");
- app.use(express.static(path.join(__dirname, "public")));
+ app.use(express.static("public"));
+ app.use(bodyParser.urlencoded({extended : true}));
+
+
+ mongoose.connect(process.env.DATABASE, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+ });
+ 
+ mongoose.connection
 
 /**
  * Routes Definitions
@@ -31,8 +46,36 @@
     res.render("index", { title: "Home" });
  });
 
- app.get("/user", (req, res) => {
+ /*app.get("/user", (req, res) => {
     res.render("user", { title: "Profile", userProfile: { nickname: "Dummy" } });
+ });*/
+
+ app.post("/",
+   [
+      check('name')
+          .isLength({ min : 1})
+          .withMessage('Please enter a name'),
+      check('email')
+          .isLength({ min : 1})
+          .withMessage('Please enter an email'),
+  ], 
+  (req, res) => {
+          const errors = validationResult(req);
+
+          if (errors.isEmpty()){
+              const registration = new Registration(req.body);
+              registration.save()
+              
+          } else{
+              res.render('form', {
+                  title: 'Resgistration form',
+                  errors: errors.array(),
+                  data: req.body,
+              });
+          }   
+   
+   console.log(req.body)
+   res.render("user", {title: "Profile", userProfile: {nickname: req.body.name}});
  });
 
 /**
